570d886f850cdf6a8e6d26b84c8a1828f6f64b38
Dannie Håkansson <danniehakan@gmail.com>
2022-09-14 20:25:33 +0200

    Changed firebase config

---
+++ README.md
@@ -0,0 +1,7 @@
+# Waiting list
+
+A waiting list meant for people to sign up for help when they need it. 
+
+Powered by Firebase.
+
+Browsable at https://list.danniehakansson.com
--- init-firebase.js
+++ init-firebase.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCqvPv_3zag5Kz085JiMc8zbU9GgnIREcs",
-  authDomain: "waiting-list-7f994.firebaseapp.com",
+  apiKey: "AIzaSyD4Euds4TQGcDRm9kkmpXnHq0pCMDQWyjg",
+  authDomain: "waiting-list-7256a.firebaseapp.com",
+  projectId: "waiting-list-7256a",
+  storageBucket: "waiting-list-7256a.appspot.com",
+  messagingSenderId: "592190998097",
+  appId: "1:592190998097:web:1a675c98f18478fa635a70",
+  measurementId: "G-92D79GVRDL",
   databaseURL:
-    "https://waiting-list-7f994-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "waiting-list-7f994",
-  storageBucket: "waiting-list-7f994.appspot.com",
-  messagingSenderId: "962130298723",
-  appId: "1:962130298723:web:ffee41c3ac9dc03a857556",
-  measurementId: "G-PPSYVGNFDR",
+    "https://waiting-list-7256a-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
 // Initialize Firebase
